93125e1702773124eede20064e85c8c0af980177
murra <murraymalik2@gmail.com>
2023-06-13 18:23:44 -0400

    Excersize variables

---
+++ Variables_Exercise1/Program.cs
@@ -0,0 +1,18 @@
+﻿//Declaring and Initalizing variables
+string name = "Malik";
+int age=26;
+char middleInitial='R';
+bool isOver18 =true;
+double currentTemp = 77.9;
+decimal currentPrice =29.99m;
+
+Console.WriteLine($"Name:{name}");
+Console.WriteLine($"Age: {age}");
+Console.WriteLine($"Middle Initial: {middleInitial}");
+Console.WriteLine($"Is Over 18: {isOver18}");
+Console.WriteLine($"Current Temp; {currentTemp}");
+Console.WriteLine($"Current Price: {currentPrice}");
+
+
+
+    
